94367519438802da62f7effc12a919628bfed240
Whoj01 <josueneto9@hotmail.com>
2022-05-08 14:26:29 -0300

    menu funcionando

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+/* selecionando pelo dom */
 
+const menuButtons = document.querySelectorAll('.toggle')
+const nav = document.querySelector('nav')
+const menuItens = document.querySelectorAll('nav ul li')
+
+function handleMenu(e) {
+  nav.classList.toggle('show')
+}
+
+menuItens.forEach(e => {
+  e.addEventListener('click', handleMenu)
+})
+
+menuButtons.forEach(e => {
+  e.addEventListener('click', handleMenu)
+})
