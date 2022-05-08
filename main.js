6045f69ffbfc5ecafa8a768c1f94b8c9ecc93b91
Whoj01 <josueneto9@hotmail.com>
2022-05-08 15:10:16 -0300

    alteração das cores do menu

--- main.js
+++ main.js
@@ -3,10 +3,21 @@
 const menuButtons = document.querySelectorAll('.toggle')
 const nav = document.querySelector('nav')
 const menuItens = document.querySelectorAll('nav ul li')
+const header = document.querySelector('#header')
 
-function handleMenu(e) {
+function handleMenu() {
   nav.classList.toggle('show')
 }
+
+function boxShadow() {
+  if (window.scrollY > 117) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+window.addEventListener('scroll', boxShadow)
 
 menuItens.forEach(e => {
   e.addEventListener('click', handleMenu)
