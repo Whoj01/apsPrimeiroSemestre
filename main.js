5156950e7620978b9b8f6c512f50278c823e9055
Whoj01 <josueneto9@hotmail.com>
2022-05-09 20:12:03 -0300

    adicionando o carrousel

--- main.js
+++ main.js
@@ -4,8 +4,37 @@ const menuButtons = document.querySelectorAll('.toggle')
 const nav = document.querySelector('nav')
 const menuItens = document.querySelectorAll('nav ul li')
 const header = document.querySelector('#header')
+const arrowNext = document.querySelectorAll('.icon-next')
+const carrouselItem = document.querySelectorAll('.carrousel-item')
+const arrowPrevious = document.querySelectorAll('.icon-previous')
+
+/* contador do carrousel */
+
+let id = 1
 
 /*Calbacks*/
+function handleCarrouselImageNext() {
+  console.log(id)
+  carrouselItem.forEach(e => {
+    e.classList.remove('visible')
+  })
+
+  if (id > carrouselItem.length - 1) {
+    id = 0
+    carrouselItem[id].classList.add('visible')
+    id++
+    console.log(id)
+  } else {
+    id++
+    carrouselItem[id].classList.add('visible')
+  }
+  /*
+  carrouselItem[id].classList.remove('visible')
+  
+  carrouselItem[id].classList.add('visible')
+  console.log(id)*/
+}
+
 function boxShadow() {
   if (window.scrollY > 130) {
     header.classList.add('scroll')
